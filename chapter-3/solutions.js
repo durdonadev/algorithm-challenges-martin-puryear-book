6807b499af82c66956505c73b7e2b031fdcb4b69
Durdona Ibragimova <durdona.dev@gmail.com>
2023-10-15 21:02:11 -0400

    Solve problems from Chapter-3

--- chapter-3/solutions.js
+++ chapter-3/solutions.js
@@ -63,3 +63,155 @@ const reverseArray2 = (array) => {
 
 console.log(reverseArray2([1, 2, 3, 4, 5, 6, 7]));
 console.log(reverseArray2([1, 2, 3, 4, 5, 6, 7, 10]));
+
+// Array: Filter Range Alan is good at breaking secret codes. One method is to eliminate values that lie within a specific known range. Given arr and values min and max, retain only the array values between min and max. Work in-place: return the array you are given, with values in original order. No built-in array functions.
+
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]     min:3     max: 7            =>     [3, 4, 5, 6, 7]
+
+function filterRangeInPlace(arr, min, max) {
+    let currentIndex = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= min && arr[i] <= max) {
+            arr[currentIndex] = arr[i]; // 5 // at index 0 [5],  at index 1 [5, 2]
+            currentIndex++;
+        }
+    }
+    console.log(currentIndex);
+
+    // Truncate the array to the new length (currentIndex)
+    arr.length = currentIndex;
+    return arr;
+}
+
+// Example usage:
+const arr = [1, 5, 2, 8, 3, 7, 4];
+const min = 2;
+const max = 6;
+
+filterRangeInPlace(arr, min, max);
+console.log(arr); // Output: [5, 2, 3, 4]
+
+// Array: Concat Replicate JavaScript’s concat(). Create a standalone function that accepts two arrays. Return a new array containing the first array’s elements, followed by the second array’s elements. Do not alter the original arrays. Ex.: arrConcat( ['a','b'], [1,2] ) should return new array ['a','b',1,2].
+
+const arrConcat = (arr1, arr2) => {
+    const newArr = [];
+
+    for (const ele of arr1) {
+        newArr.push(ele);
+    }
+
+    for (const ele of arr2) {
+        newArr.push(ele);
+    }
+    return newArr;
+};
+
+const input1 = ["a", "b"];
+const input2 = [1, 2];
+
+console.log(arrConcat(input1, input2));
+
+// Skyline Heights Lovely Burbank has a breathtaking view of the Los Angeles skyline. Let’s say you are given an array with heights of consecutive buildings, starting closest to you and extending away. Array [-1,7,3] would represent three buildings: first is actually out of view below street level, behind it is second at 7 stories high, third is 3 stories high (hidden behind the 7-story). You are situated at street level. Return array containing heights of buildings you can see, in order. Given [-1, 10, 10, 1, 5, 12, 7, 15] return [1,7]. Given [0,4] return [4]. As always with challenges, do not use built-in array functions such as unshift().
+
+// [-1, 1, 1, 7, 12, 3, 15]
+
+function visibleBuildings(heights) {
+    const visible = [];
+    let max = 0;
+
+    for (let i = 0; i < heights.length; i++) {
+        const height = heights[i];
+
+        if (height > max) {
+            visible[visible.length] = height;
+
+            // visible[0] = 1; // variable = [1];
+            // visible[1] = 7; // variable = [1, 7];
+            // visible[2] = 12; // variable = [1, 7, 12];
+            // visible[3] = 15; // variable = [1, 7, 12, 15];
+
+            max = height;
+        }
+    }
+
+    return visible;
+}
+
+console.log(visibleBuildings([-1, 1, 1, 7, 8, 9, 10, 12, 7, 8, 9, 3, 15])); // Output: [1,  7,  8, 9,10, 12, 15]
+console.log(visibleBuildings([0, 4])); // Output: [4]
+
+// Array: Remove Negatives Implement removeNegatives() that accepts an array, removes negative values, and returns the same array (not a copy), preserving non-negatives’ order. As always, do not use built-in array functions. Second: don’t use nested loops.
+
+// [2, 5, -1, 10, -3, -2, 7]
+
+const removeNegatives = (arr) => {
+    let currentIndex = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) {
+            arr[currentIndex] = arr[i];
+            currentIndex++;
+        }
+    }
+    arr.length = currentIndex;
+    return arr;
+};
+
+console.log(removeNegatives([2, 5, -1, 10, -3, -2, 7])); // [ 2, 5, 10, 7 ]
+
+// Array: Second-to-Last Return the second-to-last element of an array. Given [42,true,4,"Kate",7], return "Kate". If array is too short, return null.
+
+const secondToLast = (arr) => {
+    if (arr.length < 2) {
+        return null;
+    }
+
+    return arr[arr.length - 2];
+};
+
+console.log(secondToLast([42, true, 4, "Kate", 7])); // "Kate"
+console.log(secondToLast([42])); // null
+
+// Array: Nth-to-Last Return the element that is N-from-array’s-end. Given ([5,2,3,6,4,9,7],3), return 4. If the array is too short, return null.
+
+const nthToLast = (arr, n) => {
+    if (arr.length < 2) {
+        return null;
+    }
+
+    return arr[arr.length - n];
+};
+
+console.log(nthToLast([5, 2, 3, 6, 4, 9, 7], 3)); // output: 4
+
+// Array: Second-Largest Return the second-largest element of an array. Given [42,1,4,Math.PI,7], return 7. If the array is too short, return null.
+
+const secondLargest = (arr) => {
+    if (arr.length < 2) {
+        return null;
+    }
+
+    let maxNum = 0;
+    let secondLargest = 0;
+
+    //  [42, 1, 4, Math.PI, 7]
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > maxNum) {
+            // 42 > 0  => secondLargest = 0; maxNum = 42;
+
+            // 1 > 0 => secondLargest = 1;
+            // 4 > 1 => secondLargest = 4;
+            // 7 > 4 => secondLargest = 7;
+
+            secondLargest = maxNum;
+            maxNum = arr[i];
+        } else if (arr[i] > secondLargest) {
+            secondLargest = arr[i];
+        }
+    }
+
+    return secondLargest;
+};
+
+console.log(secondLargest([42, 1, 4, Math.PI, 7]));
