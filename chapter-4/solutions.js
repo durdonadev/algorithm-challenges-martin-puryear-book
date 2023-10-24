85fb520134c2a172a47adca81464ef463226219f
Durdona Ibragimova <durdona.dev@gmail.com>
2023-10-23 22:35:23 -0400

    Implement string acronym formatting function

--- chapter-4/solutions.js
+++ chapter-4/solutions.js
@@ -43,7 +43,7 @@ console.log(removeBlanks(" Pl ayTha tF u nkyM usi c "));
 // String: Get Digits Create a JavaScript function that given a string, returns the integer made from the string’s digits. Given "0s1a3y5w7h9a2t4?6!8?0", the function should return the number 1357924680.
 
 // unfinished needs 0 as well but Number(char) is excludes 0 because it is a falsy value.
-const getDigits1 = (string) => {
+const getDigits = (string) => {
     const strToArr = string.split("");
     const result = strToArr.filter((char) => {
         return Number(char);
@@ -51,4 +51,20 @@ const getDigits1 = (string) => {
     return result.join("");
 };
 
-console.log(getDigits1("0s1a3y5w7h9a2t4?6!8?0"));
+console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"));
+
+// Create a function that, given a string, returns the string’s acronym (first letters only, capitalized). Given " there's no free lunch  -  gotta pay yer way. ", return "TNFL-GPYW". Given "Live from New York, it's Saturday Night!", return "LFNYISN".
+
+const acronym = (str) => {
+    const capitalizedArr = str.toUpperCase().split(" ");
+    let result = [];
+
+    for (let i = 0; i < capitalizedArr.length; i++) {
+        const word = capitalizedArr[i];
+        result.push(word[0]);
+    }
+
+    return result.join("");
+};
+
+console.log(acronym(" there's no free lunch - gotta pay yer way. "));
